cd61625085c88fdb88b54cf1b329e204e79b97b9
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-09-24 09:57:37 +0700

    hapus file di folder 10 dan 11, upload baru di folder 12 dan 13

---
+++ 13-Advance-Form-Flutter/Praktikum/advance_form/lib/contacts_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Contact {
+  String name;
+  String phone;
+
+  Contact(this.name, this.phone);
+}
+
+List<Contact> contacts = [
+  Contact('Anisa Yuniarti', '+6264867252'),
+  Contact('Ararya Hafidz', '+62823412635'),
+  Contact('Delia Sepiana', '+628954109758'),
+];
